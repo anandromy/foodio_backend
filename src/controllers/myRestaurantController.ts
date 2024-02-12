d5f1765874c5342598186930103c0b8e58edf5ea
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-02-12 08:46:31 +0530

    create restaurant api

---
+++ src/controllers/myRestaurantController.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express"
+import { Restaurant } from "../models/restaurant"
+import mongoose from "mongoose"
+import { imagekit } from "../index"
+
+const createMyRestaurant = async (req: Request, res: Response) => {
+    try{
+        const existingRestaurant = await Restaurant.findOne({
+            user: req.userId
+        })
+        if(existingRestaurant){
+            return res.status(409).json({ message: "User restaurant already exists" })
+        }
+        const image = req.file as Express.Multer.File
+        const base64Image = Buffer.from(image.buffer).toString("base64")
+        const dataURI = `data:${image.mimetype};base64,${base64Image}`
+
+        imagekit.upload({
+            file : dataURI, //required
+            fileName : "test_image",   //required
+            extensions: [
+                {
+                    name: "google-auto-tagging",
+                    maxTags: 5,
+                    minConfidence: 95
+                }
+            ],
+            transformation: {
+                pre: 'l-text,i-Imagekit,fs-50,l-end',
+                post: [
+                    {
+                        type: 'transformation',
+                        value: 'w-100'
+                    }
+                ]
+            }
+        }, async function(error, result) {
+            if(error) console.log(error);
+            else{
+                const restaurant = new Restaurant(req.body)
+                restaurant.imageUrl = result?.url as string
+                restaurant.user = new mongoose.Types.ObjectId(req.userId)
+                restaurant.lastUpdated = new Date()
+                await restaurant.save()
+                res.status(201).send(restaurant)
+            }
+        });
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({ message: "Something went wrong" })
+    }
+}
+
+export default {
+    createMyRestaurant
+}
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ import cors from "cors"
 import mongoose from "mongoose"
 import "dotenv/config"
 import { router as myUserRoute } from "./routes/myUserRoutes"
+import { router as myRestaurantRoute } from "./routes/myRestaurantRoutes"
+import ImageKit from "imagekit"
+
+export let imagekit = new ImageKit({
+    publicKey : "public_7LNujhCkHeWIJCR8iWlsLcQ40pg=",
+    privateKey : "private_QmypXLgHu/9fU7lsYZ1RoSxwwkY=",
+    urlEndpoint : "https://ik.imagekit.io/786vujqar"
+});
 
 const app = express()
 const PORT = Number(process.env.PORT)
@@ -11,8 +19,10 @@ app.use(cors())
 
 app.get("/health", async(req, res) => {
     res.send({ message: "Health OK!" })
-})
+}) 
+
 app.use("/api/my/user", myUserRoute)
+app.use("/api/my/restaurant", myRestaurantRoute)
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
@@ -20,5 +30,5 @@ app.listen(PORT, () => {
 
 mongoose.connect(process.env.DATABASE_URL as string).then(() => {
     console.log("Database connected")
-})
+});
 
