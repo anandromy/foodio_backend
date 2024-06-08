31a98d35611ff2b07b7ae12e8f0eb2786820b97d
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-06-08 19:39:57 +0530

    updatemyRestaurant route

--- src/controllers/myRestaurantController.ts
+++ src/controllers/myRestaurantController.ts
@@ -66,7 +66,62 @@ const getMyRestaurant = async (req: Request, res: Response) => {
     }
 }
 
+export const updateMyRestaurant = async(req: Request, res: Response) => {
+    try{
+        const restaurant = await Restaurant.findOne({
+            user: req.userId
+        })
+        if(!restaurant){
+            return res.status(404).json({ message: "Restaurant not found" })
+        }
+        restaurant.restaurantName = req.body.restaurantName;
+        restaurant.city = req.body.city;
+        restaurant.country = req.body.country;
+        restaurant.deliveryPrice = req.body.deliveryPrice;
+        restaurant.estimatedDeliveryTime = req.body.estimatedDeliveryTime;
+        restaurant.cuisines = req.body.cuisines;
+        restaurant.menuItems = req.body.menuItems;
+        restaurant.lastUpdated = new Date();
+        if(req.file){
+            const image = req.file as Express.Multer.File
+            const base64Image = Buffer.from(image.buffer).toString("base64")
+            const dataURI = `data:${image.mimetype};base64,${base64Image}`
+            imagekit.upload({
+                file : dataURI, //required
+                fileName : "test_image",   //required
+                extensions: [
+                    {
+                        name: "google-auto-tagging",
+                        maxTags: 5,
+                        minConfidence: 95
+                    }
+                ],
+                transformation: {
+                    pre: 'l-text,i-Imagekit,fs-50,l-end',
+                    post: [
+                        {
+                            type: 'transformation',
+                            value: 'w-100'
+                        }
+                    ]
+                }
+            }, async function(error, result) {
+                if(error) console.log(error);
+                else{
+                    restaurant.imageUrl = result?.url as string
+                }
+            });
+        }
+        await restaurant.save()
+        return res.status(200).send(restaurant)
+    } catch(error) {
+        console.log(error)
+        return res.status(500).json({ message: "Something went wrong while updating restaurant" })
+    }
+}
+
 export default {
     createMyRestaurant,
-    getMyRestaurant
+    getMyRestaurant,
+    updateMyRestaurant
 }
--- src/routes/myRestaurantRoutes.ts
+++ src/routes/myRestaurantRoutes.ts
@@ -15,3 +15,4 @@ const upload = multer({
 
 router.post("/", upload.single("imageFile"), validateMyRestaurantRequest, jwtCheck, jwtParse, myRestaurantController.createMyRestaurant)
 router.get("/", jwtCheck, jwtParse, myRestaurantController.getMyRestaurant)
+router.put("/", upload.single("imageFile"), validateMyRestaurantRequest, jwtCheck, jwtParse, myRestaurantController.updateMyRestaurant)
